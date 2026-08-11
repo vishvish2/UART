64b44fc07b6b41a0f5d8c9219e483ef69e280413
vishvish2 <vishnubarath909@gmail.com>
2026-08-11 12:13:49 +0100

    Completed uart_tx_tb

--- tests/uart_tx_tb.py
+++ tests/uart_tx_tb.py
@@ -9,6 +9,12 @@ async def uart_tx_tb(dut):
     # 100MHz clock
     cocotb.start_soon(Clock(dut.clk, 10, unit="ns").start())
 
+    # Data width
+    data_width = int(dut.DATA_WIDTH.value)
+
+    # Baud count max value
+    baud_max_count = int(dut.BAUD_MAX_COUNT.value)
+
     # Assert reset for few clock cycles
     dut.rst.value = 1
     await RisingEdge(dut.clk)
@@ -17,10 +23,77 @@ async def uart_tx_tb(dut):
     dut.rst.value = 0
 
     # UART state should be IDLE and assert READY in AXI interface
-    await RisingEdge(dut.clk)
-    assert dut.tx_ready.value == 1
-
     # In IDLE, TX line should be held high
-    for _ in range(8):
+    for _ in range((data_width * baud_max_count)):  # number of clock cycles equal to data width
         await RisingEdge(dut.clk)
-        assert dut.TX.value == 1
+        assert dut.TX.value == 1, (
+            f"expected TX = 1, got TX = {dut.TX.value}"
+        )
+        assert dut.tx_ready.value == 1, (
+            f"expected tx_ready = 1, got tx_ready = {dut.tx_ready.value}"
+        )
+
+    # Test byte
+    test_bitstream = 0b01011011
+    dut.data.value = test_bitstream
+
+    # VALID not asserted, TX line should still be held high
+    for _ in range((data_width * baud_max_count)):
+        await RisingEdge(dut.clk)
+        assert dut.TX.value == 1, (
+            f"expected TX = 1, got TX = {dut.TX.value}"
+        )
+
+    # Assert AXI VALID signal for one clock cycle
+    dut.tx_valid.value = 1
+    await RisingEdge(dut.clk)                           # Wait for VALID to get recognised
+    assert dut.tx_ready.value == 1, (
+            f"expected tx_ready = 1, got tx_ready = {dut.tx_ready.value}"   # VALID and READY are 1 simultaneously
+        )
+    await RisingEdge(dut.clk)                           # Hold VALID high
+    assert dut.tx_ready.value == 0, (
+            f"expected tx_ready = 0, got tx_ready = {dut.tx_ready.value}"   # READY is deasserted, handshake successful
+        )
+    dut.tx_valid.value = 0                              # Only now deassert VALID (AXI handshake rules)
+
+    # UART state should be START, transitions to 0 for one bit period
+    for _ in range(baud_max_count - 1):                 # -1 because one clock cycle used for testing handshake
+        await RisingEdge(dut.clk)
+        assert dut.TX.value == 0, (
+            f"expected TX = 0, got TX = {dut.TX.value}"
+        )
+        assert dut.tx_ready.value == 0, (
+            f"expected tx_ready = 0, got tx_ready = {dut.tx_ready.value}"  # READY should remain deasserted
+        )
+
+    # UART state should be DATA, compare TX against data, LSB transmits first
+    for n in range(data_width):
+        for _ in range(baud_max_count):
+            expected_bit = (test_bitstream >> n) & 1    # Extract nth LSB
+            await RisingEdge(dut.clk)
+            assert dut.TX.value == expected_bit, (
+            f"expected TX = {expected_bit}, got TX = {dut.TX.value}"
+        )
+            assert dut.tx_ready.value == 0, (
+                f"expected tx_ready = 0, got tx_ready = {dut.tx_ready.value}"   # READY should remain deasserted
+            )
+
+    # UART state should now be STOP for one bit period
+    for _ in range(baud_max_count):
+        await RisingEdge(dut.clk)
+        assert dut.TX.value == 1, (
+            f"expected TX = 1, got TX = {dut.TX.value}"
+        )
+        assert dut.tx_ready.value == 0, (
+            f"expected tx_ready = 0, got tx_ready = {dut.tx_ready.value}"   # READY should remain deasserted
+        )
+
+    # UART state should return to IDLE, TX line held high and READY asserted
+    for _ in range((data_width * baud_max_count)):  # number of clock cycles equal to data width
+        await RisingEdge(dut.clk)
+        assert dut.TX.value == 1, (
+            f"expected TX = 1, got TX = {dut.TX.value}"
+        )
+        assert dut.tx_ready.value == 1, (
+            f"expected tx_ready = 1, got tx_ready = {dut.tx_ready.value}"
+        )
