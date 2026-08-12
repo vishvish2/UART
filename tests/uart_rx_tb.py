c0c7cc22f1b7152ae6531c450e9094ffd6c41dbd
vishvish2 <vishnubarath909@gmail.com>
2026-08-12 13:43:46 +0100

    Updated uart_rx_tb to test IDLE state for length of full byte transmission

--- tests/uart_rx_tb.py
+++ tests/uart_rx_tb.py
@@ -12,6 +12,10 @@ async def uart_rx_tb(dut):
     # Data width, number of bits in the data being transmitted
     data_width = int(dut.DATA_WIDTH.value)
 
+    # Baud count max value to keep track of baud ticks
+    baud_max_count_oversample = int(dut.BAUD_MAX_COUNT_OVERSAMPLE.value)
+    baud_max_count = baud_max_count_oversample * 16
+
     # Assert reset for few clock cycles
     dut.rst.value = 1
     await RisingEdge(dut.clk)
@@ -19,17 +23,21 @@ async def uart_rx_tb(dut):
     await RisingEdge(dut.clk)
     dut.rst.value = 0
 
-    assert dut.rx_valid.value == 0, (
-        f"expected rx_valid = 0, got rx_valid = {dut.rx_valid.value}"
-    )
+    # UART state should be IDLE
+    # VALID should be deasserted, data should default to zeros with no frame error flag
+    for _ in range((data_width * baud_max_count)):  # number of clock cycles equal to data width
+        await RisingEdge(dut.clk)
+        assert dut.rx_valid.value == 0, (
+            f"expected rx_valid = 0, got rx_valid = {dut.rx_valid.value}"
+        )
 
-    assert dut.data.value == 0b00000000, (
-        f"expected data = 00000000, got data = {dut.data.value}"
-    )
+        assert dut.data.value == 0b00000000, (
+            f"expected data = 00000000, got data = {dut.data.value}"
+        )
 
-    assert dut.frame_err.value == 0, (
-        f"expected frame_err = 0, got frame_err = {dut.frame_err.value}"
-    )
+        assert dut.frame_err.value == 0, (
+            f"expected frame_err = 0, got frame_err = {dut.frame_err.value}"
+        )
 
 
     
